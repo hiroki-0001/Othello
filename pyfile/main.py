dd1ff394a1c48b8d9c9873b7d366737c403c4522
hiroki-0001 <touhu.273@gmail.com>
2023-12-18 18:24:30 +0900

    デバッグ用にゲームモードとロボットモードに分けれるようにした

--- pyfile/main.py
+++ pyfile/main.py
@@ -3,23 +3,43 @@ import OthelloRecognizerSystem
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import sys
 
-hint = OthelloRecognizerSystem.Hint()
-image = cv2.imread("image/sample4.JPG")
+if len(sys.argv) == 1 or sys.argv[1] == 'game':
+    board = Board()
 
-recognizer = OthelloRecognizerSystem.Recognizer()
-ret, result = recognizer.analyzeBoard(image, hint)
-board = Board()
+elif  sys.argv[1] == 'robot':
 
-if ret:
-    # 結果を配列に格納する。-2:不明、0:空き、-1:黒、1:白
-    BoardData = np.ones((8, 8), dtype=np.int8) * 0
-    BoardData[result.isUnknown == True] = -2
-    for d in result.disc:
-        BoardData[d.cell[0], d.cell[1]] = int(d.color)
-    
-    board.read_data(BoardData)
-    board.print_info()
-    
+    hint = OthelloRecognizerSystem.Hint()
+    image = cv2.imread("image/sample1.JPG")
+    recognizer = OthelloRecognizerSystem.Recognizer()
+    ret, result = recognizer.analyzeBoard(image, hint)
+    board = Board()
+
+    if ret:
+        # 結果を配列に格納する。-2:不明、0:空き、-1:黒、1:白
+        BoardData = np.ones((8, 8), dtype=np.int8) * 0
+        BoardData[result.isUnknown == True] = -2
+        for d in result.disc:
+            BoardData[d.cell[0], d.cell[1]] = int(d.color)
+        
+        board.read_data(BoardData)
+        board.print_info()
+        
+    else:
+        print("正常に認識できませんでした")
+
 else:
-    print("正常に認識できませんでした")
+    message = '''
+使い方
+
+【オセロで遊ぶ場合】
+    python main.py 
+    または
+    python main.py game
+
+【オセロロボットで遊ぶ場合】
+    python main.py robot
+    
+    '''
+    print(message)
